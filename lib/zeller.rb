2fa13f3e16b071f700c333e583e70c3c35f0c627
Matt Knicos <mknicos@gmail.com>
2014-04-17 15:16:42 -0500

    everyday of week zeller test pass

--- lib/zeller.rb
+++ lib/zeller.rb
@@ -4,14 +4,37 @@ class Zeller
   end
 
   def self.find_start_day(month_input, year_input)
-    #day we are looking for is always the first in this case
-    first_day_of_month = 1
-    month = month_input
-    last_two_digits_of_year = year_input[-2..-1]
-    century_number = self.calc_century_number(year_input)
+
+    year = year_input.to_i
+
+    #remove leading zeros from month
+    month = month_input.delete("0").to_i
+
+    #adjust zellers congruence for jan/ feb
+    if month == 1 or month == 2
+      month += 12
+      year -= 1
+    end
+
+    year_of_century = year % 100
+    century = (year / 100).floor
+
+    day = compute_zeller(year, month, year_of_century, century)
+    day
   end
 
-  def calc_century_number(century_input)
-    
+  def self.compute_zeller(year, month, year_of_century, century)
+    #day we are looking for is always the first in this case
+    first_day_of_month = 1
+
+    part1 = ((13*(month + 1))/5)
+    part2 = year_of_century / 4
+    part3 = century / 4
+    part4 = 5 * century
+
+    answer = first_day_of_month + part1 + year_of_century + part2 + part3 + part4
+    answer = answer % 7
+    answer
   end
+
 end
--- test/test_zeller.rb
+++ test/test_zeller.rb
@@ -12,43 +12,72 @@ class TestZeller < MiniTest::Unit::TestCase
    assert_instance_of(Zeller, x)
  end
 
- def test_calc_century_number_6
-    cent_num = Zeller.calc_century_number(2000)
-    asser_equal 6, cent_num
- end
-
- def test_calc_century_number_4
-    cent_num = Zeller.calc_century_number(1900)
-    asser_equal 4, cent_num
- end
-
- def test_calc_century_number_2
-    cent_num = Zeller.calc_century_number(2600)
-    asser_equal 2, cent_num
- end
-
- def test_calc_century_number_0
-    cent_num = Zeller.calc_century_number(2700)
-    asser_equal 0, cent_num
- end
-
  def test_month_starts_on_sunday_march_2015
-   start_day = Zeller.find_start_day(03, 2015)
-   assert_equal 0, start_day
- end
-
- def test_month_starts_on_monday_jan_2001
-   start_day = Zeller.find_start_day(01, 2001)
+   start_day = Zeller.find_start_day('03', '2015')
+   puts start_day
    assert_equal 1, start_day
  end
 
- def test_month_starts_on_tues_march_201
-   start_day = Zeller.find_start_day(04, 1980)
+ def test_month_starts_on_monday_jan_2001
+   start_day = Zeller.find_start_day('01', '2001')
    assert_equal 2, start_day
  end
 
- def test_month_starts_on_wed_07
-   start_day = Zeller.find_start_day(03, 2015)
+ def test_month_starts_on_tues_march_1980
+   start_day = Zeller.find_start_day('04', '1980')
+   assert_equal 3, start_day
+ end
+
+ def test_month_starts_on_sunday_march_2015
+   start_day = Zeller.find_start_day('03', '2015')
+   assert_equal 1, start_day
+ end
+
+ def test_month_starts_on_thurs
+   start_day = Zeller.find_start_day('09', '1988')
+   assert_equal 5, start_day
+ end
+
+ def test_month_starts_on_fri
+   start_day = Zeller.find_start_day('04', '1988')
+   assert_equal 6, start_day
+ end
+
+ def test_month_starts_on_sat
+   start_day = Zeller.find_start_day('11', '2155')
    assert_equal 0, start_day
  end
+
+ def test_month_double_digit
+   start_day = Zeller.find_start_day('12', '2100')
+   assert_equal 4, start_day
+ end
+
+ #-------EXCEPTIONAL CASES---------#
+  
+ def test_feb_1800
+   start_day = Zeller.find_start_day('02', '1800')
+   assert_equal 0, start_day
+ end
+
+ def test_feb_2012
+   start_day = Zeller.find_start_day('02', '2012')
+   assert_equal 4, start_day
+ end
+
+ def test_feb_2012
+   start_day = Zeller.find_start_day('02', '1803')
+   assert_equal 3, start_day
+ end
+
+ def test_march_2014
+   start_day = Zeller.find_start_day('03', '2014')
+   assert_equal 0, start_day
+ end
+
+ def test_march_2014
+   start_day = Zeller.find_start_day('07', '1800')
+   assert_equal 3, start_day
+ end
+
 end
