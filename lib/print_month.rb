62e00d1d536ecc079f12f392afc909c247e7d425
Matt Knicos <mknicos@gmail.com>
2014-04-18 12:18:31 -0500

    return month string with number input in PrintMonth class

--- lib/print_month.rb
+++ lib/print_month.rb
@@ -1,4 +1,46 @@
 class PrintMonth
   def initialize
   end
+
+  def self.print_one_month(month, year)
+
+    #determine month string
+    month_string = ''
+
+
+  end
+
+  def self.num_spaces_for_title(month)
+  end
+
+  def self.find_month_string(month)
+    month_string = ''
+    case month
+    when 1
+      month_string = 'January'
+    when 2
+      month_string = 'February'
+    when 3
+      month_string = 'March'
+    when 4
+      month_string = 'April'
+    when 5
+      month_string = 'May'
+    when 6
+      month_string = 'June'
+    when 7
+      month_string = 'July'
+    when 8
+      month_string = 'August'
+    when 9
+      month_string = 'September'
+    when 10
+      month_string = 'October'
+    when 11
+      month_string = 'November'
+    when 12
+      month_string = 'December'
+    end
+    month_string
+  end
 end
--- test/test_print_month.rb
+++ test/test_print_month.rb
@@ -8,4 +8,27 @@ class TestPrintMonth < MiniTest::Unit::TestCase
     assert_instance_of(PrintMonth, x)
   end
 
+  #--------FIND MONTH STRING---------------#
+  
+  def test_find_month_string_January
+    month_string = PrintMonth.find_month_string(1)
+    assert_equal 'January', month_string
+  end
+
+  def test_find_month_string_March
+    month_string = PrintMonth.find_month_string(3)
+    assert_equal 'March', month_string
+  end
+
+  def test_find_month_string_December
+    month_string = PrintMonth.find_month_string(12)
+    assert_equal 'December', month_string
+  end
+
+#----------Num_spaces_for_title---------------#
+  def test_num_spaces_march
+    skip
+    spaces = PrintMonth.num_spaces_for_title('January')
+    assert_equal 5, spaces
+  end
 end
