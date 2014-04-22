e629b20b0e510a9d281f10c0d3310022b9641178
Matt Knicos <mknicos@gmail.com>
2014-04-22 09:39:16 -0500

    Error check user input, route month vs year correctly

--- test/test_cal.rb
+++ test/test_cal.rb
@@ -24,4 +24,52 @@ class TestCalIntegration < Test::Unit::TestCase
     expected_output = `cal 03 2014`
     assert_equal expected_output, output
   end
+
+  def test_july_3000
+    output = `ruby cal 07 2014`
+    expected_output = `cal 07 2014`
+    assert_equal expected_output, output
+  end
+
+  def test_dec_1979
+    output = `ruby cal 12 1979`
+    expected_output = `cal 12 1979`
+    assert_equal expected_output, output
+  end
+
+  def test_feb_1800
+    output = `ruby cal 02 1800`
+    expected_output = `cal 02 1800`
+    assert_equal expected_output, output
+  end
+
+  def test_feb_2012
+    output = `ruby cal 02 2012`
+    expected_output = `cal 02 2012`
+    assert_equal expected_output, output
+  end
+
+  def test_july_1800
+    output = `ruby cal 07 1800`
+    expected_output = `cal 07 1800`
+    assert_equal expected_output, output
+  end
+
+  def test_dec_2012
+    output = `ruby cal 12 2012`
+    expected_output = `cal 12 2012`
+    assert_equal expected_output, output
+  end
+
+  def test_feb_2015
+    output = `ruby cal 02 2015`
+    expected_output = `cal 02 2015`
+    assert_equal expected_output, output
+  end
+
+  def test_march_2015
+    output = `ruby cal 03 2015`
+    expected_output = `cal 03 2015`
+    assert_equal expected_output, output
+  end
 end
